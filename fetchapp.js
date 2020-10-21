d5764354b7c96fb3517b256c206f646da01fdbc0
Shathana <shathana.s@gmail.com>
2020-10-21 06:31:01 +0530

    committed

---
+++ fetchapp.js
@@ -0,0 +1,67 @@
+document.getElementById('button1').addEventListener('click',getText);
+
+document.getElementById('button2').addEventListener('click',getJson);
+
+document.getElementById('button3').addEventListener('click',getExternal);
+
+
+function getText(){
+    fetch('test.txt').then(function(res){
+        return res.text();
+     })
+
+     .then(function(data){
+         console.log(data);
+         //output the data
+         document.getElementById('output').innerHTML=data;
+     })
+     //error
+     .catch(function(err){
+         console.log(err)
+     })
+    }    
+
+
+
+     function getJson(){
+        fetch('post.json').then(function(res){
+            return res.json();
+         })
+    
+         .then(function(data){
+             console.log(data);
+             let output='';
+             data.forEach(function(post){
+                 output+=`<li>${post.title}<li>`;
+             });
+             document.getElementById('output').innerHTML=output;
+             //output the data
+             
+         })
+         //error
+         .catch(function(err){
+             console.log(err)
+         })
+}
+
+
+function getExternal(){
+    fetch('https://api.github.com/users').then(function(res){
+        return res.json();
+     })
+
+     .then(function(data){
+         console.log(data);
+         let output='';
+         data.forEach(function(user){
+             output+=`<li>${user.login}<li>`;
+         });
+         document.getElementById('output').innerHTML=output;
+         //output the data
+         
+     })
+     //error
+     .catch(function(err){
+         console.log(err)
+     })
+}
